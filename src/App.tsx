940fa40e295a8526bc63baf8f7d85d0c68b2f5e4
SuRyeon-Lee <bunnyatgym@gmail.com>
2023-01-08 02:27:22 +0900

    :package: add: router settting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Todo from './pages/Todo/Todo';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route index path="/" element={<Todo />} />
+          <Route index path="/auth/login?" element={<Login />} />
+          <Route index path="/auth/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
